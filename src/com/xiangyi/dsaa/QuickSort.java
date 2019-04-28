0cc159b56f0c98971478bf0f1f50376aaa0741a9
zengchao <576178421@qq.com>
2019-04-28 15:48:05 +0800

    增加注释

--- src/com/xiangyi/dsaa/QuickSort.java
+++ src/com/xiangyi/dsaa/QuickSort.java
@@ -30,16 +30,21 @@ public class QuickSort implements Sort{
         rsort(a,lp+1,e);
     }
     private int p(int[] a,int s,int e,int x){
+        //本例以最右边元素作为对比位，所以必须先从左边开始搜索
         int lp=s-1,rp=e;
         while(true){
+            //从左边开始搜索小于对比位的第一个值
             while (a[++lp]<x){}
+            //从右边开始搜索大于对比位的第一个值
             while (rp>0 && a[--rp]>x){}
             if(lp>=rp){
                 break;
             }
             x(a,lp,rp);
         }
+        //最后交换对比为与左搜索的值
         x(a,lp,e);
+        //返回分割位置
         return lp;
     }
     private void x(int[] a ,int i,int j){
--- src/com/xiangyi/dsaa/ShellSort.java
+++ src/com/xiangyi/dsaa/ShellSort.java
@@ -22,11 +22,16 @@ public class ShellSort implements Sort{
         while (3*h+1<array.length){
             h=3*h+1;
         }
+
+        //缩小间隔h
         while (h>0){
+            //间隔内每个子序列做插入排序
             for(int i=0;i<h;i++){
+                //插入排序
                 for(int m=h+i;m<array.length;m+=h){
                     int t=array[m];
                     int j=m;
+                    //向右复制
                     while (j>=h&&t<array[j-h]){
                         array[j]=array[j-h];
                         j-=h;
